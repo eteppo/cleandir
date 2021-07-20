25816b1b1b5752d86a0a1d2847913a1114636754
Eero Teppo <eero.teppo@protonmail.com>
2021-07-21 00:32:29 +0300

    improved logging and safety

--- README.md
+++ README.md
@@ -2,23 +2,19 @@
 
 ## Description
 
-A short bash shell script for cleaning unorganized backup directories of images, videos, and any other files.
+A short bash/shell script for cleaning up unorganized directories of images and videos (and any other files).
 
 IMPORTANT: ALWAYS HAVE A BACKUP BEFORE MAKING IRREVERSIBLE CHANGES.
 
 Procedures:
 
-1. Flatten the directory.
-2. Remove empty directories.
-3. Remove exact duplicates (md5sum).
-4. Organize files by file extension and the last modification year.
+1. Flatten directory by moving all files to the root and removing empty directories.
+2. Remove exact duplicates using md5sum.
+3. Organize files by the file extension and last modification year.
 
 ## Usage
 
 ```bash
-# tip: use a wide terminal window to fit progress echoes on one line
 git clone https://github.com/eteppo/cleandir
-cd cleandir
-chmod +x clean-directory.sh
-./clean-directory.sh /full/path/to/root-directory
+bash ./cleandir/clean-directory.sh /full/path/to/target-directory
 ```
--- clean-directory.sh
+++ clean-directory.sh
@@ -1,28 +1,35 @@
 #!/bin/bash
+set -e
 
-flatten_directory() {
+move_all_to_root() {
     local current_root=$1
     # loop through things in current root
-    local things=($current_root/*)
+    local things=("$current_root"/*)
     for thing in "${things[@]}"; do
-    	echo -ne "\r\033[0K$thing"
         # if thing is a directory
         if [[ -d "$thing" ]]; then
             # clean directory name if needed
-            local directory_name=$(echo "${thing// /-}" | tr '[:upper:]' '[:lower:]')
+            local directory_name
+            directory_name=$(echo "${thing// /-}" | tr '[:upper:]' '[:lower:]')
             if [[ "$thing" != "$directory_name" ]]; then
                 mv -T "$thing" "$directory_name"
             fi
             # if directory is not empty
             # apply the function to the directory (recursion)
             if [[ -n $(ls -A "$directory_name") ]]; then
-                flatten_directory "$directory_name"
+                move_all_to_root "$directory_name"
             fi
         # if thing is a file
         elif [[ ! -d "$thing" ]]; then
+            # log info
+            local counter=$(( counter + 1 ))
+            local item_name
+            item_name=$( basename "$thing" )
+            echo -ne "\r\033[0KFile $counter: $item_name "
             # clean file name
-            local filename=$(basename "${thing// /-}" | tr '[:upper:]' '[:lower:]')
-            # take $global_root from the scope of the clean_directory function
+            local filename
+            filename=$(basename "${thing// /-}" | tr '[:upper:]' '[:lower:]')
+            # take "$global_root" from the scope of the clean_directory function
             local filepath="$global_root/$filename"
             # move file to target file path
             if [[ ! -f "$filepath" ]]; then
@@ -36,28 +43,42 @@ flatten_directory() {
 }
 
 remove_empty_directories() {
-	local empty_count=$(find $global_root -empty -type d | wc -l)
+    local empty_count
+	empty_count=$(find "$global_root" -empty -type d | wc -l)
 	while [[ "$empty_count" != "0" ]]; do
-		find $global_root -empty -type d -exec rm --dir '{}' +
-		local empty_count=$(find $global_root -empty -type d | wc -l)
+		find "$global_root" -empty -type d -exec rm --dir '{}' +
+		empty_count=$(find "$global_root" -empty -type d | wc -l)
 	done
 }
 
+flatten_directory() {
+    local global_root=$1
+    local counter=0
+    move_all_to_root "$global_root"
+    remove_empty_directories "$global_root"
+}
+
 remove_exact_duplicates() {
-    local things=($global_root/*)
+    local things=( "$global_root"/* )
     # loop over files and remove if the same md5sum as before
     declare -A count_table
+    local counter=0
     for thing in "${things[@]}"; do
-    	echo -ne "\r\033[0K$thing"
-        local checksum=($(md5sum $thing))
+        # log info
+        local counter=$(( counter + 1 ))
+        local item_name
+        item_name=$( basename "$thing" )
+        echo -ne "\r\033[0KFile $counter: $item_name "
+        local checksum
+        checksum=$( md5sum "$thing" )
         # for first pass count is 1 (false), for next passes 2+ (true)
-        if ((count_table[$checksum[0]]++)); then
+        if (( count_table[$checksum[0]]++ )); then
             rm "$thing"
         fi
     done
     # remove name-conflict tags
+    local things=("$global_root"/*)
     for thing in "${things[@]}"; do
-    	echo -ne "\r\033[0K$thing"
         if [[ "$thing" =~ "£" ]]; then
             mv -T "$thing" "${thing//£}"
         fi
@@ -65,41 +86,72 @@ remove_exact_duplicates() {
 }
 
 organize_files() {
-    local files=($global_root/*)
+    local files=( "$global_root"/* )
+    local counter=0
     for file in "${files[@]}"; do
-    	echo -ne "\r\033[0K$file"
-        local modification_time=($(stat --format=%y "$file"))
-        local date=($(echo "${modification_time[0]//-/ }"))
-        local year=$(echo $date)
-        local filename=$(basename $file)
-        local extension="${filename##*.}"
-        local filepath="$global_root/$extension/$year/$filename"
-        mkdir --parents `dirname "$filepath"`
-        mv -T "$file" "$filepath"
+        if [[ ! -d "$file" ]]; then
+            local counter=$(( counter + 1 ))
+            local item_name
+            item_name=$(basename "$file")
+        	echo -ne "\r\033[0KFile $counter: $item_name "
+            local modification_time
+            modification_time=( "$( stat --format=%y "$file" )" )
+            local date
+            date="${modification_time[0]//-/ }"
+            local year
+            year="$date"
+            local filename
+            filename=$(basename "$file")
+            local extension
+            extension="${filename##*.}"
+            # use 'unknown' for missing extensions
+            if [[ -z "$extension" ]]; then
+                extension="unknown"
+            fi
+            local filepath
+            filepath="$global_root/$extension/$year/$filename"
+            mkdir --parents "$(dirname "$filepath")"
+            mv -T "$file" "$filepath"
+        fi
     done
 }
 
 clean_directory() {
     local input=$1
-    # clean global root directory names
+    # use lower case paths without spaces
     global_root=$(echo "${input// /-}" | tr '[:upper:]' '[:lower:]')
     if [[ "$input" != "$global_root" ]]; then
     	mkdir --parents "$global_root"
         mv -T "$input" "$global_root"
     fi
-    echo "Moving files to input directory..."
+    local n_files
+    n_files=$(ls --recursive --classify "$global_root" | grep -c \\*)
+    printf "Input directory has %s files.\n" "$n_files"
+    printf "Flattening directory...\n"
     flatten_directory "$global_root"
-    echo -e "Files moved to input directory.\n"
-    echo "Removing empty directories..."
-    remove_empty_directories "$global_root"
-    echo -e "Empty directories removed.\n"
-    echo "Removing exact duplicates..."
+    printf "\nDone.\n"
+    printf "Removing exact duplicates...\n"
     remove_exact_duplicates "$global_root"
-    echo -e "Exact duplicates removed.\n"
-    echo "Organizing files by extension and last modification year..."
+    printf "\nDone.\n"
+    printf "Organizing files by file extension and the last modification year...\n"
     organize_files "$global_root"
-    echo -e "Files organized by extension and last modification year.\n"
-    echo -e "Done.\nResults in $global_root."
+    printf "\nDone.\n"
+    n_files=$(ls --recursive --classify "$global_root" | grep -c \\*)
+    printf "Output directory has %s files.\n" "$n_files"
+    printf "Output directory: %s\n" "$global_root"
 }
 
-clean_directory "$1"
+input_path=$1
+if [[ ! -d "$input_path" ]]; then
+    # print to standard error
+    echo "Input path is not a directory." >&2; exit 1
+fi
+printf "Input directory: %s\n" "$input_path"
+while true; do
+    read -r -p "Are you sure [y|n]? " yn
+    case $yn in
+        [Yy]* ) clean_directory "$input_path"; break;;
+        [Nn]* ) exit 0;;
+        * ) echo "Please type 'yes' or 'no' and press enter.";;
+    esac
+done
